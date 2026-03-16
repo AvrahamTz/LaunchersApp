e365b025a55217c0c93fcca71c093c04797cb7cf
AB <abtzoref@gmail.com>
2026-03-16 11:04:51 +0200

    gitignore added

--- server/src/controllers/launchers.controllers.js
+++ server/src/controllers/launchers.controllers.js
@@ -6,7 +6,7 @@ export const getAllLaunchers = async (req,res) => {
      const allLaunchers = await db.collection("launchers").find().toArray();
      res.send(allLaunchers);
    } catch (error) {
-       return  res.status(401).json({messeage:error});
+       return  res.status(500).json({messeage:error});
    }
 };
 
@@ -15,24 +15,27 @@ export const getALauncher = async (req,res) => {
      const Launcher = await db.collection("launchers").findOne({_id:new ObjectId(req.path)}).toArray();
      res.send(Launcher);
    } catch (error) {
-       return  res.status(401).json({messeage:error});
+       return  res.status(500).json({messeage:error});
    }
 };
 
 export const addALauncher = async (req,res) => {
     const {city,rocketType,latitude,longitude,name} =req.body;
+    if (!latitude.isInteger() || !longitude.isInteger()){
+        res.status(400).json({msg:"the coordinates must be a number"});
+    }
     const launcher ={
          city,
          rocketType,
-         latitude,
-         longitude,
+         latitude:Number(latitude),
+         longitude:Number(longitude),
          name
     };
    try {
      const addLauncherToDB = await db.collection("launchers").insertOne(launcher);
      res.send(addLauncherToDB);
    } catch (error) {
-       return  res.status(401).json({messeage:error});
+       return  res.status(500).json({messeage:error});
    }
 };
 
@@ -41,7 +44,7 @@ export const deleteALauncher = async (req,res) => {
      const deleteLauncher = await db.collection("launchers").deleteOne({_id:new ObjectId(req.params)});
      res.send(deleteLauncher);
    } catch (error) {
-       return  res.status(401).json({messeage:error});
+       return  res.status(500).json({messeage:error});
    }
 };
 
---
+++ server/src/routers/launchers.routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { addALauncher, deleteALauncher, getALauncher, getAllLaunchers } from "../controllers/launchers.controllers.js";
+const router = express.Router();
+
+router.get("/",getAllLaunchers);
+router.get("/id",getALauncher);
+router.post("/",addALauncher);
+router.delete("/:id",deleteALauncher);
+

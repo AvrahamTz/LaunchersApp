f46dac963a251ac5588f8aa4233056821f0a6cdf
AB <abtzoref@gmail.com>
2026-03-17 18:20:49 +0200

    second try

--- server/src/DB/db.js
+++ server/src/DB/db.js
@@ -8,4 +8,20 @@ try {
 } catch (err) {
     console.error(err)
 }
+Dbinit = async ()=>{
+    const admin = await client.db("IDF_TEST").collection("users").findOne({user_type:"admin"})
+    if(!admin){
+        const newAdmin = {
+            username:"Avraham",
+            password:"123!qwr",
+            email:"abcd@gmail.com",
+            user_type:"admin",
+            last_login:null
+        }
+        const initAdmin = await client.db("IDF_TEST").collection("users").insertOne(newAdmin)
+        if(initAdmin){
+            console.log("admin created sucssefully");
+        }
+    }
+}
 export const db = client.db("IDF_TEST");
--- server/src/server.js
+++ server/src/server.js
@@ -2,12 +2,14 @@ import express from 'express';
 import {config} from 'dotenv';
 import cors from 'cors';
 import LaunchersRoutes from './routers/launchers.routes.js'
+import authRouters from './routers/auth.routes.js'
 config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use ("/api/launchers",LaunchersRoutes)
+app.use("/api/auth",authRouters)
 app.listen(process.env.PORT,() => {
     console.log(`server running at localhost${process.env.PORT}`);
 });
